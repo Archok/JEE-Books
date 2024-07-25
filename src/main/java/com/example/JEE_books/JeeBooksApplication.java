b99d8ad0372a71853b2e07c6a66a095b60dd1ef9
Nicolas <beauchard.nicolas@gmail.com>
2024-07-25 16:41:17 +0200

    Initial commit

---
+++ src/main/java/com/example/JEE_books/JeeBooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.JEE_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JEE_books/JeeBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JEE_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
